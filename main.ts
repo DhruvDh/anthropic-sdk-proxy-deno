4efc8d3cd3d9db9dcd5e3bde8d2b429193b73103
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2024-11-09 12:18:18 +0000

    Revert to e41b04b

--- main.ts
+++ main.ts
@@ -1,21 +1,12 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import Anthropic from "npm:@anthropic-ai/sdk";
-import { AnthropicVertex } from "npm:@anthropic-ai/vertex-sdk";
-import { GoogleAuth } from "npm:google-auth-library";
 
-const auth = new GoogleAuth({ apiKey: Deno.env.get("VERTEX_API_KEY") ?? "" });
 const router = new Router();
 const app = new Application();
 
 // Initialize Anthropic client
-// const anthropic = new Anthropic({
-//   apiKey: Deno.env.get("ANTHROPIC_API_KEY") ?? "",
-//   defaultHeaders: {
-//     "anthropic-beta": "prompt-caching-2024-07-31",
-//   },
-// });
-const anthropic = new AnthropicVertex({
-  apiKey: Deno.env.get("VERTEX_API_KEY") ?? "",
+const anthropic = new Anthropic({
+  apiKey: Deno.env.get("ANTHROPIC_API_KEY") ?? "",
   defaultHeaders: {
     "anthropic-beta": "prompt-caching-2024-07-31",
   },
